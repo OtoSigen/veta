deba3b8abb609bae14e10a7aaa4e05b11147d01c
Maksim Akinshin <otosigen@gmail.com>
2020-10-23 14:44:37 +0300

    wip

--- dist/js/script.js
+++ dist/js/script.js
@@ -70,3 +70,94 @@ const burgerOpen = () => {
   });
 };
 burgerOpen();
+
+// formValidate = () => {
+//   const form = document.querySelector('#validate-form')
+//   const nameValue = document.querySelector('#popup-name-form')
+//   const phoneValue = document.querySelector('#popup-phone-form')
+//   const emailValue = document.querySelector('#popup-email-form')
+//   const fields = document.querySelectorAll('.field')
+//   const btn = document.querySelector('#popup__btn-post')
+
+
+//   form.addEventListener('submit', function (event) {
+//     event.preventDefault()
+
+//     let errors = form.querySelectorAll('.error')
+
+//     for (var i = 0; i < errors.length; i++) {
+//       errors[i].remove()
+//     }
+
+//     fields.forEach((item) => {
+//       if(!item.value){
+//         let error = document.createElement('div')
+//         error.className='error'
+//         error.classList.add = 'error'
+//         error.innerHTML = 'Поле не может быть пустым'
+//         item.parentElement.insertBefore(error, item)
+//       }
+//     })
+
+//     if(nameValue.length || emailValue.length || phoneValue.length <0){
+//       btn.setAttribute("disabled", "disabled");
+//     }
+    
+//   })
+  
+// }
+// formValidate()
+
+Validate = () => {
+  const form = document.querySelector('#validate-form')
+  form.addEventListener('submit', formSend)
+
+  async function formSend(e){
+    e.preventDefault();
+ 
+
+  let error = formValidate(form)
+
+}
+
+function formValidate (form){
+  let error = 0;
+  let formReq = document.querySelectorAll('._req')
+  
+  for (let index = 0; index < formReq.length; index++){
+    const input = formReq[index];
+    formRemoveError(input);
+  
+  if(input.classList.contains('email')){
+    if(emailTest(input)){
+      formAddError(input)
+      error++
+    }
+    else if(input.getAttribute('type') === 'checkbox' && input.checked === false){
+      formAddError(input)
+      error++
+    }
+    else {
+      if(input.value === ''){
+        formAddError(input)
+        error++
+      }
+    }
+   }
+  }
+}
+
+function formAddError(input)  {
+  input.parentElement.classList.add('error');
+  input.classList.add('error');
+}
+function formRemoveError(input)  {
+  input.parentElement.classList.remove('error');
+  input.classList.remove('error');
+}
+// function emailTest(input){
+//   return
+// }
+}
+
+Validate()
